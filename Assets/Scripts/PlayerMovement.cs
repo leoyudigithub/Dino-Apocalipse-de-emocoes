64fd7e9e5c7ca91e93a343ff4797d821615924c8
leoyudigithub <leonardoyudicoelho@gmail.com>
2025-10-15 12:14:09 -0300

    player-movement_colision-walls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMovement
+public class PlayerMovement : MonoBehaviour
 {
-    
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    //public Animator animator;
+    public float speed = 2f;
+
+    private void Start()
+    {
+        //Ao iniciar o jogo, acessa o Rigidbody2D do personagem
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+
+
+        moveH = Input.GetAxis("Horizontal") * speed;
+        moveV = Input.GetAxis("Vertical") * speed;
+
+        rb.linearVelocity = new Vector2(moveH, moveV);
+       
+
+     
+    }
 }
