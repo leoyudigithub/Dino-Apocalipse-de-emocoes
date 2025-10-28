2c23b36fa29270ec099701716e038769cb296c6b
Laboratório FIAP <leonardoyudicoelho@gmail.com>
2025-10-28 12:49:55 -0300

    ponto de ataque vira na direcao do jogador

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,47 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float moveH, moveV;
-    //public Animator animator;
     public float speed = 2f;
-    
+    public Transform pontoAtaque;
+
+    private bool olhandoDireita = true;
+    private Vector3 pontoAtaqueLocalOriginal;
 
     private void Start()
     {
-        //Ao iniciar o jogo, acessa o Rigidbody2D do personagem
         rb = GetComponent<Rigidbody2D>();
+
+        if (pontoAtaque != null)
+            pontoAtaqueLocalOriginal = pontoAtaque.localPosition;
     }
 
     private void FixedUpdate()
     {
-
-
-        moveH = Input.GetAxis("Horizontal") * speed;
-        moveV = Input.GetAxis("Vertical") * speed;
+        moveH = Input.GetAxisRaw("Horizontal") * speed;
+        moveV = Input.GetAxisRaw("Vertical") * speed;
 
         rb.linearVelocity = new Vector2(moveH, moveV);
-        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
 
-        //flip o sprite
-        if( moveH > 0 )
-        {
-            
-            sprite.flipX = true;
-            
-            
-        }
-        else if( moveH < 0 )
-        {
-            sprite.flipX = false;
-        }
-        
-        
+        if (moveH > 0 && !olhandoDireita)
+            Virar();
+        else if (moveH < 0 && olhandoDireita)
+            Virar();
+    }
 
-     
+    private void Virar()
+    {
+        olhandoDireita = !olhandoDireita;
+
+        // Inverte o personagem inteiro (isso faz o AttackPoint acompanhar automaticamente)
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
+
+        // Corrige o ponto de ataque, se quiser manter uma distância lateral fixa
+        if (pontoAtaque != null)
+        {
+            pontoAtaque.localPosition = new Vector3(
+                pontoAtaqueLocalOriginal.x * (olhandoDireita ? 1 : -1),
+                pontoAtaqueLocalOriginal.y,
+                pontoAtaqueLocalOriginal.z
+            );
+        }
     }
 }
